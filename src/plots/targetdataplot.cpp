74436ab2b74d914febc6272eafcc2229899766c7
Patrick Würflinger <wuerflingerp@gmx.at>
2020-12-24 20:27:02 +0100

    added visible area to targetplot

--- src/plots/targetdataplot.cpp
+++ src/plots/targetdataplot.cpp
@@ -11,12 +11,11 @@ void TargetDataPlot::update(const Targets_t &data)
     m_target_data_series->clear();
 
     for (auto & e: data)
-        m_target_data_series->append(QPointF(e.azimuth, e.radius / 100));
+        m_target_data_series->append(QPointF(e.azimuth, e.radius/100));
 }
 
 void TargetDataPlot::initialize()
 {
-
     m_target_data_series->setName("Detected targets");
     m_target_data_series->setMarkerSize(12);
     m_target_data_series->setColor(Qt::red);
@@ -29,7 +28,7 @@ void TargetDataPlot::initialize()
     auto angularAxis = new QValueAxis();
     angularAxis->setRange(-180, 180);
     angularAxis->setTickAnchor(180);
-    angularAxis->setTickCount(9);
+    angularAxis->setTickCount(13);
     angularAxis->setLabelFormat("%d" + QString::fromLatin1("°"));
     addAxis(angularAxis, QPolarChart::PolarOrientationAngular);
     m_target_data_series->attachAxis(angularAxis);
@@ -44,4 +43,46 @@ void TargetDataPlot::initialize()
     QFont font;
     font.setPixelSize(20);
     setTitleFont(font);
+
+    QLineSeries *seriesleft1 = new QLineSeries();
+    seriesleft1->attachAxis(angularAxis);
+    seriesleft1->attachAxis(radialAxis);
+    seriesleft1->append(0,0);
+    seriesleft1->append(55, 100);
+
+    QLineSeries *seriesright1 = new QLineSeries();
+    seriesright1->attachAxis(angularAxis);
+    seriesright1->attachAxis(radialAxis);
+    seriesright1->append(0,0);
+    seriesright1->append(-180, 100);
+
+    QLineSeries *seriesleft2 = new QLineSeries();
+    seriesleft2->attachAxis(angularAxis);
+    seriesleft2->attachAxis(radialAxis);
+    seriesleft2->append(0,0);
+    seriesleft2->append(-55, 100);
+
+    QLineSeries *seriesright2 = new QLineSeries();
+    seriesright2->attachAxis(angularAxis);
+    seriesright2->attachAxis(radialAxis);
+    seriesright2->append(0,0);
+    seriesright2->append(-180, 100);
+
+    QAreaSeries * area1 = new QAreaSeries(seriesleft1, seriesright1);
+    area1->setBorderColor(Qt::white);
+    area1->setOpacity(0.5);
+    addSeries(area1);
+    area1->setBrush(QColor("#c0000000"));
+    area1->attachAxis(angularAxis);
+    area1->attachAxis(radialAxis);
+    legend()->markers(area1)[0]->setVisible(false);
+
+    QAreaSeries * area2 = new QAreaSeries(seriesleft2, seriesright2);
+    area2->setBorderColor(Qt::white);
+    area2->setOpacity(0.5);
+    addSeries(area2);
+    area2->setBrush(QColor("#c0000000"));
+    area2->attachAxis(angularAxis);
+    area2->attachAxis(radialAxis);
+    legend()->markers(area2)[0]->setVisible(false);
 }
